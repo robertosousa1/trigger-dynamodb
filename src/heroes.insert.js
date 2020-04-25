0d96938c80abaf0c0c85935a92d4c54f047d3ecb
Roberto <roberto_junior_sousa@hotmail.com>
2020-04-24 23:33:03 -0300

    function for insertion in dynamodb

---
+++ src/heroes.insert.js
@@ -0,0 +1,73 @@
+const uuid = require('uuid')
+const Joi = require('@hapi/joi')
+const decoratorValidator = require('./util/decoratorValidator')
+const globalEnum = require('./util/globalEnum')
+
+class Handler {
+  constructor({ dynamoDbSvc }) {
+    this.dynamoDbSvc = dynamoDbSvc
+    this.dynamodbTable = process.env.DYNAMODB_TABLE
+  }
+
+  static validator() {
+    return Joi.object({
+      name: Joi.string().max(100).min(2).required(),
+      power: Joi.string().max(20).required()
+    })
+  }
+
+  prepareData(data) {
+    const params = {
+      TableName: this.dynamodbTable,
+      Item: {
+        ...data,
+        id: uuid.v1(),
+        createdAt: new Date().toISOString()
+      }
+    }
+    return params
+  }
+
+  async insertItem(params) {
+    return this.dynamoDbSvc.put(params).promise()
+  }
+
+  handlerSuccess(data) {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+    return response
+  }
+
+  handlerError(data) {
+    return {
+      statusCode: data.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create item!'
+    }
+  }
+
+  async main(event) {
+    try {
+      const data = event.body
+      const dbParams = this.prepareData(data)
+      await this.insertItem(dbParams)
+      return this.handlerSuccess(dbParams.Item)
+    } catch (error) {
+      console.error('Error**', error.stack)
+      return this.handlerError({ statusCode: 500 })
+    }
+  }
+}
+
+const AWS = require('aws-sdk')
+const dynamoDB = new AWS.DynamoDB.DocumentClient()
+const handler = new Handler({
+  dynamoDbSvc: dynamoDB
+})
+module.exports = decoratorValidator(
+  handler.main.bind(handler),
+  Handler.validator(),
+  globalEnum.ARG_TYPE.BODY
+)
